94cb646247da7e2363b1a987a299c4db50affaee
Thomas Darmawan <tdarmawan@hacktiv8.com>
2024-01-01 23:21:49 +0700

    test CI/CD #3

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /will', function() {
-    it('respond with hello world', function(done) {
+    it('respond with hello world, my name is thomas darmawan', function(done) {
         request(app).get('/will').expect('{ "response": "Hello World" }', done);
     });
 });
